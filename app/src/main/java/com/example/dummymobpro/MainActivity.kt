ca4939b6b9b182729128986e21a97d49383fdb14
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-05-18 23:43:45 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/com/example/dummymobpro/MainActivity.kt
+++ app/src/main/java/com/example/dummymobpro/MainActivity.kt
@@ -1,22 +1,10 @@
 package com.example.dummymobpro
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.dummymobpro.ui.screen.MainScreen
 import com.example.dummymobpro.ui.theme.DummyMobproTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,47 +19,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
-        }
-    ) { innerPadding ->
-    ScreenContent(Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES,showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    DummyMobproTheme {
-        MainScreen()
-    }
-}
