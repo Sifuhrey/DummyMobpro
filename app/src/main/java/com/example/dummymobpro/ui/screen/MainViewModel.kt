a2201765954b9aa1d14cfca0e2d0c48a18a68031
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-05-19 00:45:27 +0700

    Membuat tampilan nama hewan

--- app/src/main/java/com/example/dummymobpro/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/example/dummymobpro/ui/screen/MainViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.dummymobpro.ui.screen
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.dummymobpro.model.Hewan
 import com.example.dummymobpro.network.HewanApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
+    var data = mutableStateOf(emptyList<Hewan>())
+        private set
     init {
         retrieveData()
     }
@@ -15,8 +19,7 @@ class MainViewModel: ViewModel() {
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "success: $result")
+               data.value = HewanApi.service.getHewan()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "failure: ${e.message}")
             }
